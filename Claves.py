3811f9495f11a9b7f8f7d23ca9710ce1b01344dd
Peio <pe.tola@aulanz.net>
2023-02-21 16:44:00 +0100

    Pasword

---
+++ Claves.py
@@ -0,0 +1,7 @@
+while True:
+    pasw1=input("Teclea tu contraseña: ")
+    pasw2=input("teclea de nuevo tu contraseña:")
+    if pasw1==pasw2:
+        print("Claves idénticas")
+        break
+    print("Claves diferentes")
