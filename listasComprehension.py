11a091d25ff6fffc78eef2f48dfb78f101210552
AtilaReyDeLosHunos <125383305+AtilaReyDeLosHunos@users.noreply.github.com>
2023-02-22 16:53:44 +0100

    Add files via upload

---
+++ listasComprehension.py
@@ -0,0 +1,5 @@
+numeros=[3,54,-12,4,-67,99,-120]
+lista_negativa=[valor for valor in numeros if valor < 1]
+print(lista_negativa)
+lista_positiva=[valor for valor in numeros if valor >1]
+print(lista_positiva)
