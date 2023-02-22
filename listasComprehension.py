604cc9e08df5d144cc62a1710b4d8249fc976f92
Peio <pe.tola@aulanz.net>
2023-02-22 17:12:14 +0100

    Cambio de datos

--- listasComprehension.py
+++ listasComprehension.py
@@ -1,4 +1,4 @@
-numeros=[3,54,-12,4,-67,99,-120]
+numeros=[3,54,-12,4,-67,99,-120,584]
 lista_negativa=[valor for valor in numeros if valor < 1]
 print(lista_negativa)
 lista_positiva=[valor for valor in numeros if valor >1]
