c9436b1ae9b21b6e0ed0d5fa70d32dd691704a53
Peio <pe.tola@aulanz.net>
2023-02-22 17:07:54 +0100

    otra confirmacion

---
+++ Cambiodetexto.py
@@ -0,0 +1,19 @@
+texto = "Pitón es un lenguaje de programación. Pitón está usado para la automación, análisis de datos e incluso la creación de páginas webs. Pitón fue creado por Bill Gates en 1960. Pitón es difícil de usar."
+texto=texto.replace("Pitón","Python")
+
+texto=texto.replace("Bill Gates","Guido Van Rossum")
+
+texto=texto.replace("1960","1989")
+
+texto=texto.replace("difícil","fácil")
+print(texto)
+texto5 = "    Lo más importante que nos ha mantenido en Python... bueno, hay 2 cosas importantes. Uno son las bibliotecas. La otra cosa que nos mantiene en Python, y esto es lo más importante, es facil de leer y entender. Cuando contratamos nuevos empleados. Solo digo, 'todo lo que escribas debe estar en python'. Sólo para que pueda leerlo. Y es increíble porque puedo ver desde el otro lado de la habitación, mirando su pantalla, si su código es bueno o malo. Porque un buen código de Python tiene una estructura muy obvia. Y eso hace que mi vida sea mucho más fácil        "
+valor1=texto5.count("Python")
+print(valor1)
+valor2=texto5.count("python")
+print(valor2)
+
+dato=texto5.find("Python")
+print(dato)
+    
+    
