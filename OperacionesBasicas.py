e696be6279c3c630c54b8454f193161fd3a0c6ea
Peio <pe.tola@aulanz.net>
2023-02-23 15:59:37 +0100

    Operaciones

---
+++ OperacionesBasicas.py
@@ -0,0 +1,11 @@
+def sumar(n1,n2):
+    return n1+n2
+def restar(n1,n2):
+    return n1-n2
+n1=float(input("Teclea el primer número"))
+n2=float(input("Teclea el segundo valor"))
+if __name__=="__main__":
+    x=sumar(8,2)
+    print(x)
+    print(sumar(n1,n2))
+    print(restar(n1,n2))
