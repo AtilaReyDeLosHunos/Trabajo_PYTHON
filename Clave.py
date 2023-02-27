ed1753db64e1fcf6d3adf96b164b0c6658db02ad
Peio <pe.tola@aulanz.net>
2023-02-27 15:41:49 +0100

    Cambios

--- Clave.py
+++ Clave.py
@@ -1,18 +1,32 @@
+LISTA_DE_ERRORES=["123456","password","Password","contraseña"]
+nombre_valido=False
+clave_valida=False
 
-respuesta="S"
+def validar_nombre(nombre):
+    entero = int(len(nombre))
+    return entero > 5
 
-while respuesta=="S":
-    nombre=input("Teclea tu nombre(mayor de 5 letras)")
-    mayusculas=nombre.capitalize()
-    clave=input("Teclea tu clave")
-    if len(nombre) <5 or len(clave)<6 or clave =="123456" or clave=="password" or clave=="contraseña":
+def validar_clave(clave):
+    longitud=int(len(clave))
+    if clave not in LISTA_DE_ERRORES and longitud > 6:
+        return " "      
         
-        print("Error")
-        respuesta=input("Deseas cambiar los datos(S/N)")
-    else:
-        respuesta=input("Deseas introducir otros datos(S/N)")
-        print(f"El nombre es: {nombre} y la clave es: {clave}")
-        print(f"Bienvenido {mayusculas}")
+while not nombre_valido:
+    nombre=input("Teclea tu nombre(mayor de 5 letras)")
+    nombre_valido=validar_nombre(nombre)
+    longitud_nombre=len(nombre)
+    if not nombre_valido:
+        print('Lo siento, el nombre no es válido, vuelva a intentarlo: ')
+
+while not clave_valida:
+    clave=input("Teclea tu clave: ")
+    clave_valida=validar_clave(clave)
+    longitud_clave=len(clave)
+    if not clave_valida:
+         print('Lo siento, la clave no es válido, vuelva a intentarlo: ')
+
+print(f'El nombre {nombre} de {longitud_nombre} letras, es válido.', end="\n")
+print(f'La clave {clave}  de {longitud_clave} digitos es válida.', end="\n")   
 
     
 
