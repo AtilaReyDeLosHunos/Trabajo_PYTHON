b08ca60a0ad70b24217d33111c85f71d12ea336b
Peio <pe.tola@aulanz.net>
2023-02-27 16:33:15 +0100

    Ok

--- Importar_os.py
+++ Importar_os.py
@@ -1,7 +1,7 @@
 import os
 import platform
-print(os.name)
-print(platform.uname())
+#print(os.name)
+#print(platform.uname())
 print(os.getcwd())
 path=os.path.join(os.getcwd(),"CursoPython")
 print(path)
@@ -9,7 +9,7 @@ print(os.path.split(os.getcwd()))
 print(os.path.isfile(os.path.join(os.getcwd(),"Alumnos.py")))
 print(os.listdir(path))
 os.chdir("../")
-os.mkdir("test1")
+#os.mkdir("test1")
 os.listdir(path)
 
 
