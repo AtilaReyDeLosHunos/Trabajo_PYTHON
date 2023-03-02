7806fa56ca261c89019f3c24db4202bede16d4d2
Peio <pe.tola@aulanz.net>
2023-03-02 15:21:26 +0100

    actualizo

--- import_csv.py
+++ import_csv.py
@@ -1,7 +1,8 @@
 import csv
-with open("address.csv" , 'r') as f:
+
+with open("C:\Trabajo_PYTHON\direcciones.csv",'r') as f:
     csvreader = csv.reader (f)
     print (type (csvreader))
     for row in csvreader:
-        print (row[ 0], row[ 4])
-        print (row)
+        print (row[0], row[4])
+        #print (row)
